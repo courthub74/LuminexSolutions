715c70692aa646d4b8912b6f94e7bf6c38674d20
Courdevelops <courdevelops@gmail.com>
2024-04-29 16:39:44 -0400

    ready for footer

--- insights.js
+++ insights.js
@@ -27,11 +27,14 @@ let box_four = document.getElementById('box_four_words');
 square_one.addEventListener('mouseover', () => {
     // change the style of the words
     box_one.classList.add('appear');
+    // change the box shadow
+    w_square_one.classList.add('hover');
 });
 
 // to mouseout take mouse away
 square_one.addEventListener('mouseout', () => {
     box_one.classList.remove('appear');
+    w_square_one.classList.remove('hover');
 });
 
 // for phone touch 'pointerdown'
@@ -40,6 +43,8 @@ w_square_one.addEventListener('pointerdown', () => {
     box_one.classList.toggle('appear');
     // Make box expand
     w_square_one.classList.toggle('pressed');
+    // Change box shadow
+    w_square_one.classList.toggle('touch');
 });
 
 // Two
@@ -47,11 +52,14 @@ w_square_one.addEventListener('pointerdown', () => {
 square_two.addEventListener('mouseover', () => {
     // change the style of the words
     box_two.classList.add('appear');
+    // change the box shadow
+    w_square_two.classList.add('hover');
 });
 
 // to mouseout take mouse away
 square_two.addEventListener('mouseout', () => {
     box_two.classList.remove('appear');
+    w_square_two.classList.remove('hover');
 });
 
 // to click on phone 
@@ -60,6 +68,8 @@ w_square_two.addEventListener('pointerdown', () => {
     box_two.classList.toggle('appear');
     // Make box expand
     w_square_two.classList.toggle('pressed');
+    // change the box shadow
+    w_square_two.classList.toggle('touch');
 });
 
 // Three
@@ -67,11 +77,14 @@ w_square_two.addEventListener('pointerdown', () => {
 square_three.addEventListener('mouseover', () => {
     // change the style of the words
     box_three.classList.add('appear');
+    // change the box shadow
+    w_square_three.classList.add('hover');
 });
 
 // to mouseout take mouse away
 square_three.addEventListener('mouseout', () => {
     box_three.classList.remove('appear');
+    w_square_three.classList.remove('hover');
 });
 
 // to click on phone 
@@ -80,6 +93,8 @@ w_square_three.addEventListener('pointerdown', () => {
     box_three.classList.toggle('appear');
     // Make box expand
     w_square_three.classList.toggle('pressed');
+    // change the box shadow
+    w_square_three.classList.toggle('touch');
 });
 
 // Four
@@ -87,11 +102,14 @@ w_square_three.addEventListener('pointerdown', () => {
 square_four.addEventListener('mouseover', () => {
     // change the style of the words
     box_four.classList.add('appear');
+    // change the box shadow
+    w_square_four.classList.add('hover');
 });
 
 // to mouseout take mouse away
-button_four.addEventListener('mouseout', () => {
+square_four.addEventListener('mouseout', () => {
     box_four.classList.remove('appear');
+    w_square_four.classList.remove('hover');
 });
 
 // to click on phone 
@@ -100,4 +118,6 @@ w_square_four.addEventListener('pointerdown', () => {
     box_four.classList.toggle('appear');
     // Make box expand
     w_square_four.classList.toggle('pressed');
+    // change the box shadow
+    w_square_four.classList.toggle('touch');
 });
